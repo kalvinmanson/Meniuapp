468dfa2766ce7cba6407b9d48f61c6006130d7d1
Kalvin Manson <kalvinmanson@gmail.com>
2018-02-03 13:34:06 -0500

    create places views

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -111,8 +111,8 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new(place_params)
     @place.user = current_user
-    @place.visits = 0
     @place.rank = 0
+    @place.views = 0
     if @place.save
       redirect_to place_path(@place), notice: 'Place was successfully created.'
     else
